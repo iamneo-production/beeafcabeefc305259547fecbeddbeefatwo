a5ba4812c8ee97c90ec805ecc7f3a38722ce5d3d
Iamneo <techadmin@iamneo.ai>
2023-09-20 08:07:04 +0000

    Backup Commit

---
+++ KathakBookingSystem/Models/KathakClassBookingException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KathakBookingSystem.Models
+{
+    // Write your KathakClassBookingException here...
+        public class KathakClassBookingException : Exception
+    {
+        public KathakClassBookingException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ KathakBookingSystem/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KathakBookingSystem.Models
+{
+    // Write your Student class here...
+
+       public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ClassID { get; set; }
+        public Class Class { get; set; }
+    }
+}
