4365aa4bca8a9ecde85ebc08507f9f7a2af8e91a
Iamneo <techadmin@iamneo.ai>
2023-09-20 12:16:24 +0000

    Backup Commit

--- KathakBookingSystem/Controllers/BookingController.cs
+++ KathakBookingSystem/Controllers/BookingController.cs
@@ -8,30 +8,89 @@ namespace KathakBookingSystem.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public BookingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ClassEnrollmentForm(int id)
         {
-            // Write ClassEnrollmentForm functionality here
-            // if students count is greater than Class Capacity Throw a exception "Class is fully booked."
-            // if class not found return NotFound() else return View()
-            return null;
+            var selectedClass = _context.Classes.Find(id);
+            var selectedClass1 = _context.Classes
+                    .Include(c => c.Students)
+                    .FirstOrDefault(c => c.ClassID == id);
+
+            Console.WriteLine(selectedClass1.Capacity);
+            Console.WriteLine(selectedClass1.Students.Count);
+
+            if (selectedClass1.Students.Count >= selectedClass1.Capacity)
+            {
+                throw new KathakClassBookingException("Class is fully booked.");
+            }
+
+            if (selectedClass == null)
+            {
+                return NotFound(); // Handle class not found
+            }
+
+            return View(selectedClass);
         }
 
         [HttpPost]
         public IActionResult ClassEnrollmentForm(int id, string name, string email)
         {
-            // Write functionality for adding students
-            // Successfull adding redirect to EnrollmentConfirmation with StudentID
-            // Handle KathakClassBookingException "Class is fully booked."
-            return null;
+            try
+            {
+                var selectedClass = _context.Classes
+                    .Include(c => c.Students)
+                    .FirstOrDefault(c => c.ClassID == id);
 
+                if (selectedClass == null)
+                {
+                    return NotFound(); // Handle class not found
+                }
+
+                if (selectedClass.Students.Count >= selectedClass.Capacity)
+                {
+                    throw new KathakClassBookingException("Class is fully booked.");
+                }
+
+                var student = new Student
+                {
+                    Name = name,
+                    Email = email,
+                    ClassID = id
+                };
+
+                _context.Students.Add(student);
+                _context.SaveChanges();
+
+                return RedirectToAction("EnrollmentConfirmation", new { studentId = student.StudentID });
+            }
+            catch (KathakClassBookingException ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(); // Return to the enrollment form with an error message
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions here, such as database errors
+                return View("Error"); // Redirect to an error page
+            }
         }
 
         public IActionResult EnrollmentConfirmation(int studentId)
         {
-            // Write functionality to display EnrollmentConfirmation student
-            // Return NotFound if null else Return View
-            return null;
+            var enrolledStudent = _context.Students.Include(s => s.Class).FirstOrDefault(s => s.StudentID == studentId);
 
+            if (enrolledStudent == null)
+            {
+                return NotFound(); // Handle student not found
+            }
+
+            return View(enrolledStudent);
         }
     }
 }
